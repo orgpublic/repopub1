74bcd479e588f1841bb6165a2e6446b337e2b11a
Pankaj Gupta <mail2panki@gmail.com>
2026-06-26 23:38:04 +0530

    Api Call Added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { Link, Route, Routes } from "react-router";
 import MainContent from "./MainContent";
-import UseStatehook from "./UseStateHook";
+import ApiCall from "./ApiCall";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
@@ -10,12 +10,12 @@ const NavBar = () => {
       <div className="bg-gray-800 text-white p-4 flex flex-row gap-4">
         <Link to="/">Home</Link>
         <Link to="/userlist">User List</Link>
-        <Link to="/usestate">UseState</Link>
+        <Link to="/apicall">ApiCall</Link>
       </div>
       <div className="flex flex-row">
         <Routes>
           <Route path="/" element={<MainContent />}></Route>
-          <Route path="/usestate" element={<UseStatehook />}></Route>
+          <Route path="/apicall" element={<ApiCall />}></Route>
           <Route path="/userlist" element={<UserList />}></Route>
           <Route path="/userlist/:id" element={<UserDetails />}></Route>
         </Routes>
